d63b9e4344bce9dd7c0c57b515d459d029dba872
JonataBecker <jonatabecker@gmail.com>
2017-05-24 20:42:29 -0300

    Create project

---
+++ src/main/java/com/github/jonatabecker/erl/NewClass.java
@@ -0,0 +1,12 @@
+package com.github.jonatabecker.erl;
+
+/**
+ *
+ * @author JonataBecker
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Buga");
+    }
+    
+}
